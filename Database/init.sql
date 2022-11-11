e2ff1d93439c876134b6155080930fc8fbc63bb0
dvdzmr <51931592+dvdzmr@users.noreply.github.com>
2022-11-11 14:19:07 +0100

    Added login functionality
    Make sure to fully delete your old local database and regen it via docker compose.
    
    You might need to do a migration, make sure to delete everything in the data/migrations folder before starting.
    
    Currently users can only be registered via the website itself, this will need to be handled from the backend at a later stage  directly and the option removed from the page.

--- Database/init.sql
+++ Database/init.sql
@@ -1,8 +1,8 @@
-CREATE SCHEMA userdata;
-CREATE TABLE userdata.logins (
-    name VARCHAR(200) NOT NULL,
-    password VARCHAR(200) NOT NULL
-);
+-- CREATE SCHEMA userdata;
+-- CREATE TABLE userdata.logins (
+--     name VARCHAR(200) NOT NULL,
+--     password VARCHAR(200) NOT NULL
+-- );
 
 CREATE SCHEMA chengeta;
 CREATE TABLE chengeta.sounds (
