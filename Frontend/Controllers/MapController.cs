0ce0faabfa511f7e3ead747946b5f0d460e5d787
SkyHitted <95493454+SkyHitted@users.noreply.github.com>
2022-11-14 14:23:49 +0100

    No Errors
    - Errors gefixt van vorige commit

--- Frontend/Controllers/MapController.cs
+++ Frontend/Controllers/MapController.cs
@@ -6,65 +6,37 @@ namespace Frontend.Controllers;
 
 public class MapController : Controller
 {
-    // GET Map/EventList
-    public async Task<IActionResult> Main()
+    public List<Map> GetEvents()
     {
-        // Krijg list van database items.
-        // var allevent = new Map();
         var getdbevents = DBconnection.DBChecker();
         List<Map> test = new List<Map>();
         for (int i = Math.Max(0, getdbevents.Count - 5); i< getdbevents.Count; i++) // getdbevent.count - 5, 5 veranderen naar variable voor filter later
         {
             var dbevent = getdbevents[i];
             var allevent = new Map()
-                {
-                    id = dbevent.id,
-                    time = dbevent.time,
-                    latitude = dbevent.latitude,
-                    longitude = dbevent.longitude,
-                    soundtype = dbevent.soundtype,
-                    probability = dbevent.probability,
-                    soundfile = dbevent.soundtype
-                };
+            {
+                id = dbevent.id,
+                time = dbevent.time,
+                latitude = dbevent.latitude,
+                longitude = dbevent.longitude,
+                soundtype = dbevent.soundtype,
+                probability = dbevent.probability,
+                soundfile = dbevent.soundtype
+            };
             test.Add(allevent);
         }
-
         test.Reverse();
-        // var testing = new Map();
-        // testing.mylist = new List<Test>()
-        // {
-        //     new Test()
-        //     {
-        //         id = 1,
-        //         probability = 50,
-        //         soundtype = "gun"
-        //     },
-        //     new Test()
-        //     {
-        //         id = 2,
-        //         probability = 40,
-        //         soundtype = "thunder"
-        //     },
-        //     new Test()
-        //     {
-        //         id = 3,
-        //         probability = 24,
-        //         soundtype = "unknown"
-        //     },
-        //     new Test()
-        //     {
-        //         id = 4,
-        //         probability = 90,
-        //         soundtype = "animal"
-        //     },
-        //     new Test()
-        //     {
-        //         id = 5,
-        //         probability = 67,
-        //         soundtype = "vehicle"
-        //     },
-        // };
-        Console.WriteLine(test[0].ToString());
+        return test;
+    }
+    public PartialViewResult RecentEvents()
+    {
+        var test = GetEvents();
+        return PartialView("RecentEvents", test);
+    }
+    // GET Map/EventList
+    public async Task<IActionResult> Main()
+    {
+        var test = GetEvents();
         return PartialView(test);
     }
 }
